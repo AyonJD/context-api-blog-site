2077a4188da4a85224ae14c1d2d92667eb6564a4
Ayon <ayonjodder177@gmail.com>
2022-04-02 00:38:09 +0600

    Data faced and home updated

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import Home from './Components/Home/Home'
 import Navbar from "./Components/Navbar/Navbar";
 import Videos from "./Components/Videos/Videos";
 import Login from "./Components/Login/Login";
+import { createContext, useState } from "react";
 
+//Creating Context for sharing faced data to all the Component
+export const BlogDataContext = createContext();
 function App() {
+  const [blogsData, setBlogsData] = useState([])
 
   return (
     <div>
+      <BlogDataContext.Provider value={[blogsData, setBlogsData]}>
       <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/videos" element={<Videos></Videos>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/videos" element={<Videos></Videos>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+        </Routes>
+      </BlogDataContext.Provider>
     </div>
   )
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,25 @@
+import { useContext, useEffect } from "react";
+import { BlogDataContext } from "../../App";
 import Blog from "../Blog/Blog";
 import "./Home.css";
 
 const Home = () => {
+  //Calling the Context to set the faced data to the state declared in App.js
+  const [blogsData, setBlogsData] = useContext(BlogDataContext);
+  useEffect(() => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => setBlogsData(data))
+  }, [])
 
   return (
-    // <div className='blogs-container'>
-    //   {blogs.map((blog, index) => (
-    //     <Blog key={index} blog={blog} />
-    //   ))}
-    // </div>
-    <h1>Home page</h1>
+    <>
+      <div className='blogs-container'>
+      {blogsData.map((blog, index) => (
+        <Blog key={index} blog={blog} />
+      ))}
+      </div>
+    </>
   );
 };
 
